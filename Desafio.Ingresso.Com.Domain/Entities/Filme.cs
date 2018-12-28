0a97964a90e32033be13a091f8d5f3ce37584267
SamuMarques <samuelmarquescruz@hotmail.com>
2018-12-28 00:05:11 -0200

    - Entidade Filme - Criacao do contexto

---
+++ Desafio.Ingresso.Com.Domain/Entities/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio.Ingresso.Com.Domain.Entities
+{
+    public class Filme
+    {
+        public string IdFilme { get; set; }
+        public string Nome { get; set; }
+        public string Classificacao { get; set; }
+        public string Genero { get; set; }
+        public string Sinopse { get; set; }
+    }
+}
---
+++ Desafio.Ingresso.Com.Infra.Data/Contexto/Context.cs
@@ -0,0 +1,26 @@
+﻿using Desafio.Ingresso.Com.Domain.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio.Ingresso.Com.Infra.Data.Contexto
+{
+    public class Context
+    {
+        private readonly IMongoDatabase database;
+
+        public Context()
+        {
+            database = new MongoClient("mongodb://admin:desenv1@ds113873.mlab.com:13873/samuelsdb").GetDatabase("samuelsdb");
+        }
+
+        public IMongoCollection<Filme> Posts
+        {
+            get
+            {
+                return database.GetCollection<Filme>("Filmes");
+            }
+        }
+    }
+}
